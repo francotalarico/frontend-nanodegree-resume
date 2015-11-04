4836e7845e2459c89d7a7b5f6ac9b34f9f3269c4
Franco Talarico <francotalarico93@gmail.com>
2015-11-04 15:24:52 -0300

    BIo quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Franco Talarico";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole).prepend(formattedName);
+
+var skills = ['UX Designer', 'GIT', 'Python', 'JS', 'AngularJS'];
+
+var bio = {
+    'name': 'Franco Talarico',
+    'role': 'Web developer',
+    'contacts': {
+        'email': 'francotalarico93@gmail.com',
+        'tel': 152037032,
+        'address': 'Paraguay 4693'
+    },
+    'picture': 'images/me.png',
+    'welcomeMessage': 'Welcome!',
+    'skills': skills
+};
+
+$("#main").append(bio.name);
